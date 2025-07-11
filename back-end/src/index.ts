57da21322a1f8269973161395e70dba1ff3c3143
IRekik <isrekik.2000@hotmail.com>
2025-07-11 22:02:42 +0900

    - Add NODE_ENV environment variable - Add middleware to automatically log API endpoint calls - Made all extra non-error logging messages print conditionnaly on dev mode for all endpoints

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import http from "http";
 
-import apiRouter from "./routes/api"
+import apiRouter from "./routes/api";
+import requestLogger from "./middleware/requestLogger";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use(requestLogger);
 app.use("/api", apiRouter);
 
 const httpServer = http.createServer(app);
