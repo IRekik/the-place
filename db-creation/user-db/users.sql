8209fd9799a220062fc6fd82748d291f7893de8e
IRekik <isrekik.2000@hotmail.com>
2025-12-01 22:32:57 +0900

    [DB] Add first name, last name and pseudo to users record

--- db-creation/user-db/users.sql
+++ db-creation/user-db/users.sql
@@ -5,5 +5,8 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     admin BOOLEAN DEFAULT FALSE,
     roles TEXT[] DEFAULT '{}',
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    pseudo VARCHAR(50) NOT NULL;
     created_at TIMESTAMP DEFAULT NOW()
 );
