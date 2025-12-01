693e734c834fc0704288abe00f0c22679ffe8db1
IRekik <isrekik.2000@hotmail.com>
2025-12-01 21:49:18 +0900

    [UI] Add Login button to layout navbar

--- front-end/components/home/Navbar.tsx
+++ front-end/components/home/Navbar.tsx
@@ -4,10 +4,20 @@ import Link from "next/link";
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container ml-4">
+      <div className="container mx-auto flex items-center justify-between">
         <Link href="/">
-          <h1 className="text-white text-2xl font-bold">ThePlace</h1>
+          <h1 className="text-white text-2xl font-bold cursor-pointer">
+            ThePlace
+          </h1>
         </Link>
+
+        <div>
+          <Link href="/login">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+              Login
+            </button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
